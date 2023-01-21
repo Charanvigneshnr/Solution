ff1f97952ff7c1651ed3bfe4565a4951008b2230
Charan Vignesh NR <charanvignesh10@gmail.com>
2023-01-21 23:45:01 +0530

    Create README.md

---
+++ src/Charan/X.java
@@ -0,0 +1,21 @@
+package Charan;
+
+public class X {
+    static void LetterX(String s, int len) {
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                if ((i==j)||(i+j==len-1))
+                {
+                    System.out.print(s.charAt(i));
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
