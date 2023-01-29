8e477175d075f4fdf7ec0cf27f28eddcf65799b3
charanvigneshnr <charanvignesh10@gmail.com>
2023-01-29 23:35:42 +0530

    Refactored

--- src/Charan/PrintAlphabets/Main.java
+++ src/Charan/PrintAlphabets/Main.java
@@ -9,34 +9,33 @@ public class Main
         int StrLen=s.length();
         System.out.print("Enter the alphabet to print: ");
         char Alphabet=sc.next().charAt(0);
-        switch (Alphabet)
-        {
-            case 'A':A.LetterA(s, StrLen);break;
-            case 'B':B.LetterB(s, StrLen);break;
-            case 'C':C.LetterC(s, StrLen);break;
-            case 'D':D.LetterD(s, StrLen);break;
-            case 'E':E.LetterE(s, StrLen);break;
-            case 'F':F.LetterF(s, StrLen);break;
-            case 'G':G.LetterG(s, StrLen);break;
-            case 'H':H.LetterH(s, StrLen);break;
-            case 'I':I.LetterI(s, StrLen);break;
-            case 'J':J.LetterJ(s, StrLen);break;
-            case 'K':K.LetterK(s, StrLen);break;
-            case 'L':L.LetterL(s, StrLen);break;
-            case 'M':M.LetterM(s, StrLen);break;
-            case 'N':N.LetterN(s, StrLen);break;
-            case 'O':O.LetterO(s, StrLen);break;
-            case 'P':P.LetterP(s, StrLen);break;
-            case 'Q':Q.LetterQ(s, StrLen);break;
-            case 'R':R.LetterR(s, StrLen);break;
-            case 'S':S.LetterS(s, StrLen);break;
-            case 'T':T.LetterT(s, StrLen);break;
-            case 'U':U.LetterU(s, StrLen);break;
-            case 'V':V.LetterV(s, StrLen);break;
-            case 'W':W.LetterW(s, StrLen);break;
-            case 'X':X.LetterX(s, StrLen);break;
-            case 'Y':Y.LetterY(s, StrLen);break;
-            case 'Z':Z.LetterZ(s, StrLen);break;
+        switch (Alphabet) {
+            case 'A' -> A.LetterA(s, StrLen);
+            case 'B' -> B.LetterB(s, StrLen);
+            case 'C' -> C.LetterC(s, StrLen);
+            case 'D' -> D.LetterD(s, StrLen);
+            case 'E' -> E.LetterE(s, StrLen);
+            case 'F' -> F.LetterF(s, StrLen);
+            case 'G' -> G.LetterG(s, StrLen);
+            case 'H' -> H.LetterH(s, StrLen);
+            case 'I' -> I.LetterI(s, StrLen);
+            case 'J' -> J.LetterJ(s, StrLen);
+            case 'K' -> K.LetterK(s, StrLen);
+            case 'L' -> L.LetterL(s, StrLen);
+            case 'M' -> M.LetterM(s, StrLen);
+            case 'N' -> N.LetterN(s, StrLen);
+            case 'O' -> O.LetterO(s, StrLen);
+            case 'P' -> P.LetterP(s, StrLen);
+            case 'Q' -> Q.LetterQ(s, StrLen);
+            case 'R' -> R.LetterR(s, StrLen);
+            case 'S' -> S.LetterS(s, StrLen);
+            case 'T' -> T.LetterT(s, StrLen);
+            case 'U' -> U.LetterU(s, StrLen);
+            case 'V' -> V.LetterV(s, StrLen);
+            case 'W' -> W.LetterW(s, StrLen);
+            case 'X' -> X.LetterX(s, StrLen);
+            case 'Y' -> Y.LetterY(s, StrLen);
+            case 'Z' -> Z.LetterZ(s, StrLen);
         }
     }
 }
