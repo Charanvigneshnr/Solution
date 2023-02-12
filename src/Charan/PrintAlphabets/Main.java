96e5bf5148a7f7847410f2385aa7606225eaf5fa
Charanvigneshnr <charanvignesh10@gmail.com>
2023-02-12 16:00:37 +0530

    Learning Collections Framework

--- src/Charan/PrintAlphabets/Main.java
+++ src/Charan/PrintAlphabets/Main.java
@@ -1,41 +1,41 @@
-package Charan.PrintAlphabets;
-import java.util.Scanner;
-public class Main
-{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter a string of odd length: ");
-        String s = sc.nextLine();
-        int StrLen=s.length();
-        System.out.print("Enter the alphabet to print: ");
-        char Alphabet=sc.next().charAt(0);
-        switch (Alphabet) {
-            case 'A' -> A.LetterA(s, StrLen);
-            case 'B' -> B.LetterB(s, StrLen);
-            case 'C' -> C.LetterC(s, StrLen);
-            case 'D' -> D.LetterD(s, StrLen);
-            case 'E' -> E.LetterE(s, StrLen);
-            case 'F' -> F.LetterF(s, StrLen);
-            case 'G' -> G.LetterG(s, StrLen);
-            case 'H' -> H.LetterH(s, StrLen);
-            case 'I' -> I.LetterI(s, StrLen);
-            case 'J' -> J.LetterJ(s, StrLen);
-            case 'K' -> K.LetterK(s, StrLen);
-            case 'L' -> L.LetterL(s, StrLen);
-            case 'M' -> M.LetterM(s, StrLen);
-            case 'N' -> N.LetterN(s, StrLen);
-            case 'O' -> O.LetterO(s, StrLen);
-            case 'P' -> P.LetterP(s, StrLen);
-            case 'Q' -> Q.LetterQ(s, StrLen);
-            case 'R' -> R.LetterR(s, StrLen);
-            case 'S' -> S.LetterS(s, StrLen);
-            case 'T' -> T.LetterT(s, StrLen);
-            case 'U' -> U.LetterU(s, StrLen);
-            case 'V' -> V.LetterV(s, StrLen);
-            case 'W' -> W.LetterW(s, StrLen);
-            case 'X' -> X.LetterX(s, StrLen);
-            case 'Y' -> Y.LetterY(s, StrLen);
-            case 'Z' -> Z.LetterZ(s, StrLen);
-        }
-    }
-}
+//package Charan.PrintAlphabets;
+//import java.util.Scanner;
+//public class Main
+//{
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("Enter a string of odd length: ");
+//        String s = sc.nextLine();
+//        int StrLen=s.length();
+//        System.out.print("Enter the alphabet to print: ");
+//        char Alphabet=sc.next().charAt(0);
+//        switch (Alphabet) {
+//            case 'A' -> A.LetterA(s, StrLen);
+//            case 'B' -> B.LetterB(s, StrLen);
+//            case 'C' -> C.LetterC(s, StrLen);
+//            case 'D' -> D.LetterD(s, StrLen);
+//            case 'E' -> E.LetterE(s, StrLen);
+//            case 'F' -> F.LetterF(s, StrLen);
+//            case 'G' -> G.LetterG(s, StrLen);
+//            case 'H' -> H.LetterH(s, StrLen);
+//            case 'I' -> I.LetterI(s, StrLen);
+//            case 'J' -> J.LetterJ(s, StrLen);
+//            case 'K' -> K.LetterK(s, StrLen);
+//            case 'L' -> L.LetterL(s, StrLen);
+//            case 'M' -> M.LetterM(s, StrLen);
+//            case 'N' -> N.LetterN(s, StrLen);
+//            case 'O' -> O.LetterO(s, StrLen);
+//            case 'P' -> P.LetterP(s, StrLen);
+//            case 'Q' -> Q.LetterQ(s, StrLen);
+//            case 'R' -> R.LetterR(s, StrLen);
+//            case 'S' -> S.LetterS(s, StrLen);
+//            case 'T' -> T.LetterT(s, StrLen);
+//            case 'U' -> U.LetterU(s, StrLen);
+//            case 'V' -> V.LetterV(s, StrLen);
+//            case 'W' -> W.LetterW(s, StrLen);
+//            case 'X' -> X.LetterX(s, StrLen);
+//            case 'Y' -> Y.LetterY(s, StrLen);
+//            case 'Z' -> Z.LetterZ(s, StrLen);
+//        }
+//    }
+//}
