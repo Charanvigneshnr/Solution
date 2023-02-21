c47b4102e439f950b1739f011cd44c8f56e32575
Charanvigneshnr <charanvignesh10@gmail.com>
2023-02-21 19:51:27 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Charan/PrintAlphabets/Main.java

--- src/Charan/LeetCode/GroupAnagrams.java
+++ src/Charan/LeetCode/GroupAnagrams.java
@@ -3,27 +3,26 @@ import java.util.*;
 public class GroupAnagrams
 {
         public List<List<String>> groupAnagrams(String[] strs) {
-            List<List<String>> outerList = new ArrayList<List<String>>();
-            List<String> innerList = new ArrayList<String>();
-            int i=0;
-            int j=1;
+            List<List<String>> outerList = new ArrayList<>();
+            List<String> innerList = new ArrayList<>();
+            int i;
+            int j;
             int len = strs.length;
             for(i=0;i<len-1;i++)
             {
                 for (j=i+1;j<len;j++)
                 {
-                    if ((checkAnagram(strs[i],strs[j])==false)&&j==len-1)
+                    if ((!checkAnagram(strs[i], strs[j]))&&j==len-1)
                     {
                         innerList.add(strs[i]);
-                        outerList.add(innerList);
                         break;
                     }
                     else
                     {
                         innerList.add(strs[j]);
                     }
-                    outerList.add(innerList);
                 }
+                outerList.add(innerList);
             }
             return outerList;
         }
@@ -34,7 +33,7 @@ public class GroupAnagrams
             int len2=t.length();
             char[] arr1 = s.toCharArray();
             char[] arr2 = t.toCharArray();
-            Boolean flag = false;
+            boolean flag = false;
             if (len1==len2)
             {
                 Arrays.sort(arr1);
