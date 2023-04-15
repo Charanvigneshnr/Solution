82e01dd09050aca7a38bc042fe32dbc6b122d995
Charanvigneshnr <charanvignesh10@gmail.com>
2023-04-15 09:22:44 +0530

    Added explanation

--- src/Charan/LeetCode/MaxValueOfCoins.java
+++ src/Charan/LeetCode/MaxValueOfCoins.java
@@ -22,3 +22,11 @@ public class MaxValueOfCoins
         return mv[k];
     }
 }
+
+//        This code solves a problem of finding the maximum total number of coins that can be obtained from a given set of piles of coins, where at most k coins can be taken from each pile.
+//        The input consists of a list of lists piles, where each inner list represents a pile of coins. The integer k specifies the maximum number of coins that can be taken from each pile.
+//        The function maxValueOfCoins calculates the maximum number of coins that can be obtained by using a dynamic programming approach. It initializes two arrays, mv and pileSum, both of length k + 1.
+//        The pileSum array keeps track of the sum of coins up to a certain index for each pile. For each pile, the sum is calculated up to the minimum of the size of the pile and the maximum number of coins k.
+//        The mv array keeps track of the maximum total number of coins that can be obtained using up to i coins, where i ranges from 1 to k. The outer loop iterates over all the piles of coins. For each pile, the inner loop iterates over the mv array backwards from k to 1, and calculates the maximum number of coins that can be obtained using up to i coins from the current pile and the previously processed piles.
+//        The maximum number of coins that can be obtained using up to i coins from the current pile and the previously processed piles is calculated by taking the maximum of the current sum of coins up to j from the current pile and the maximum value of coins that can be obtained using i - j coins from the previous piles.
+//        Finally, the function returns the maximum total number of coins that can be obtained using up to k coins.
