f09a16eabdc021147d074d5dec9d256c7743adb6
Charanvigneshnr <charanvignesh10@gmail.com>
2023-04-22 13:47:02 +0530

    Leetcode hard problem: Restore array

--- src/Charan/LeetCode/CountPrimes.java
+++ src/Charan/LeetCode/CountPrimes.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 public class CountPrimes {
     public int CountPrimes(int n) {
         if (n <= 2) return 0;
-        boolean[] primes = new boolean[n+1];
+        boolean[] primes = new boolean[n + 1];
         Arrays.fill(primes, true);
         primes[0] = false;
         primes[1] = false;
-        int i,j,len = (int) Math.sqrt(n) + 1, count = 0;
+        int i, j, len = (int) Math.sqrt(n) + 1, count = 0;
         // Iterate over all numbers from 2 to the square root of n.
         // For each number i, if it is prime (i.e., primes[i] is true),
         // set all multiples of i (i.e., primes[j] where j is a multiple of i) to false.
-        for (i = 2; i*i <= n; i++) {
+        for (i = 2; i * i <= n; i++) {
             if (primes[i]) {
                 count++;
-                for (j = i*i; j <= n; j += i) {
+                for (j = i * i; j <= n; j += i) {
                     primes[j] = false;
                 }
             }
