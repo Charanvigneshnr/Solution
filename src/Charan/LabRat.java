12c6bad163602ff0c01e9d776f0a5db265b5b8fd
Charanvigneshnr <charanvignesh10@gmail.com>
2023-05-16 11:35:49 +0530

    Graph

--- src/Charan/LabRat.java
+++ src/Charan/LabRat.java
@@ -3,32 +3,39 @@ package Charan;
 import java.util.Arrays;
 
 public class LabRat {
-    public int[][] generateMatrix(int n) {
-        int[][] matrix = new int[n][n];
+    public static void main(String[] args) {
 
-        for (int[] row : matrix) {
-            Arrays.fill(row, 0);
+//        int[][] matrix = new int[n][n];
+//
+//        for (int[] row : matrix) {
+//            Arrays.fill(row, 0);
+//        }
+//
+//        matrix[0][n / 2] = 1;
+//        int i = 0;
+//        int j = n / 2;
+//        int count = 2;
+//
+//        for (int num = 2; num <= n * n; num++) {
+//            int newI = (i - 1 + n) % n;
+//            int newJ = (j + 1) % n;
+//
+//            if (matrix[newI][newJ] == 0) {
+//                i = newI;
+//                j = newJ;
+//            } else {
+//                i = (i + 1) % n;
+//            }
+//
+//            matrix[i][j] = num;
+//        }
+//
+//        return matrix;
+
+        int[] arr1 = {1,2,3,4,5,6,7,8,9,10};
+        for(int x:arr1)
+        {
+            System.out.print(x+" ");
         }
-
-        matrix[0][n / 2] = 1;
-        int i = 0;
-        int j = n / 2;
-        int count = 2;
-
-        for (int num = 2; num <= n * n; num++) {
-            int newI = (i - 1 + n) % n;
-            int newJ = (j + 1) % n;
-
-            if (matrix[newI][newJ] == 0) {
-                i = newI;
-                j = newJ;
-            } else {
-                i = (i + 1) % n;
-            }
-
-            matrix[i][j] = num;
-        }
-
-        return matrix;
     }
 }
--- src/Charan/Solution.java
+++ src/Charan/Solution.java
@@ -1,15 +1,81 @@
 package Charan;
 
-import Charan.*;
-import Charan.Recursion.Recursion;
-
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) {
-        Recursion r = new Recursion();
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        int res= r.result(arr, 4);
-        System.out.println(res);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of nodes: ");
+        int n = sc.nextInt();
+        System.out.println("Enter number of edges: ");
+        int m = sc.nextInt();
+        int i, j;
+        int[][] ip = new int[n][m];
+        for (i = 0; i < m; i++) {
+            System.out.print("Node A: ");
+          ip[i][0]=sc.nextInt();
+            System.out.print("Node B: ");
+          ip[i][1]=sc.nextInt();
+        }
+        for (i = 0; i < n; i++) {
+            System.out.print(i+"->");
+            for (j = 0; j < m; j++) {
+                if (ip[j][0] == i) {
+                    System.out.print(ip[j][1]+", ");
+                }
+                else if (ip[j][1] == i) {
+                    System.out.print(ip[j][0]+", ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
+
+//        System.out.print("Enter number of nodes: ");
+//        int n = sc.nextInt();
+//        System.out.print("Enter number of edges: ");
+//        int m = sc.nextInt();
+//        System.out.print("Is directed?:");
+//        int c = sc.nextInt();
+//        int[][] arr = new int[n + 1][m + 1];
+//        int i;
+//        int nodeA = 0;
+//        int nodeB = 0;
+//        int weight = 0;
+//        switch (c) {
+//            case 0:
+////                    undirected graph
+//                for (i = 0; i < m; i++) {
+//                    System.out.print("Enter from node: ");
+//                    nodeA = sc.nextInt();
+//                    System.out.print("Enter to node: ");
+//                    nodeB = sc.nextInt();
+//                    System.out.print("Enter weight: ");
+//                    weight = sc.nextInt();
+//                    arr[nodeA][nodeB] = weight;
+//                    arr[nodeB][nodeA] = weight;
+//                }
+//                break;
+//            case 1:
+////                    directed graph
+//                for (i = 0; i < m; i++) {
+//                    System.out.print("Enter from node: ");
+//                    nodeA = sc.nextInt();
+//                    System.out.print("Enter to node: ");
+//                    nodeB = sc.nextInt();
+//                    System.out.print("Enter weight: ");
+//                    weight = sc.nextInt();
+//                    arr[nodeA][nodeB] = weight;
+//                }
+//                break;
+//            default:
+//                System.out.println("Invalid choice");
+//                break;
+//        }
+//        for (i = 1; i <= n; i++) {
+//            for (int j = 1; j <= m; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
