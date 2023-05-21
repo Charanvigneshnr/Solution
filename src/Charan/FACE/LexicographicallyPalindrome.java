ab501dbf6fdeedc937bd38d8b49711b29295119e
Charanvigneshnr <charanvignesh10@gmail.com>
2023-05-21 16:14:03 +0530

    Foobar4a started

--- src/Charan/FACE/LexicographicallyPalindrome.java
+++ src/Charan/FACE/LexicographicallyPalindrome.java
@@ -1,6 +1,30 @@
 package Charan.FACE;
 
-public class LexicographicallyPalindrome {
-//    MAMA : AM|AM true
+import java.util.Arrays;
 
+public class LexicographicallyPalindrome {
+    public boolean isLexicographicallyPalindrome(String s) {
+        int len = s.length();
+        int pos = len / 2;
+        if (len % 2 != 0) {
+            pos = len / 2 + 1;
+        }
+        boolean res = true;
+        char[] s1 = new char[len / 2];
+        for (int i = 0; i < len / 2; i++) {
+            s1[i] = s.charAt(i);
+        }
+        Arrays.sort(s1);
+        for (int i = 0; i < len / 2; i++) {
+            s1[i] = s.charAt(pos + i);
+        }
+        Arrays.sort(s1);
+        for (int i = 0; i < len / 2; i++) {
+            if (s1[i] != s.charAt(i)) {
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
 }
---
+++ src/Charan/FACE/Solution.java
@@ -0,0 +1,8 @@
+package Charan.FACE;
+
+public class Solution {
+    public static void main(String[] args) {
+        LexicographicallyPalindrome lp = new LexicographicallyPalindrome();
+        System.out.println(lp.isLexicographicallyPalindrome("ABCDCBA"));
+    }
+}
