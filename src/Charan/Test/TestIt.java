919fd74dcfd12f0677ba43e5b0652fb4e465d1d0
Charanvigneshnr <charanvignesh10@gmail.com>
2023-05-27 13:29:36 +0530

    DP

--- src/Charan/Test/TestIt.java
+++ src/Charan/Test/TestIt.java
@@ -3,7 +3,7 @@ package Charan.Test;
 public class TestIt {
     public static void main(String[] args) {
         LabRat lb = new LabRat();
-        String[] arr = {"Ate", "Ace", "Girl"};
+        String[] arr = {"Ate", "Ace", "Ama"};
         System.out.println(lb.findStringVowelFirstAndLast(3, arr));
     }
 }
