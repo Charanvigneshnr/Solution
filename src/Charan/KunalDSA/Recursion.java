d0f84d54511932be46ccb49dc1e9e11d0424a05e
Charanvigneshnr <charanvignesh10@gmail.com>
2023-05-31 00:58:31 +0530

    Recursion

--- src/Charan/KunalDSA/Recursion.java
+++ src/Charan/KunalDSA/Recursion.java
@@ -1,18 +1,20 @@
 package Charan.KunalDSA;
 
 public class Recursion {
-    static int number = 0;
-
     public static void main(String[] args) {
-        recursion(12045);
-        System.out.println(number);
+        recursion(4, 4);
     }
 
-    public static void recursion(int n) {
-        if (n == 0) {
+    public static void recursion(int r, int c) {
+        if (r == 0) {
             return;
         }
-        number = number * 10 + n % 10;
-        recursion(n / 10);
+        if (r < c) {
+            System.out.print("*");
+            recursion(r + 1, c);
+        } else {
+            System.out.println();
+            recursion(0, c - 1);
+        }
     }
 }
