3b2c03fd614f84f229be2b9318bf1e3f08d1280d
Charanvigneshnr <charanvignesh10@gmail.com>
2023-06-02 23:43:41 +0530

    Foobar4a not working yet Recursion Subsequence

--- src/Charan/KunalDSA/Recursion.java
+++ src/Charan/KunalDSA/Recursion.java
@@ -2,19 +2,19 @@ package Charan.KunalDSA;
 
 public class Recursion {
     public static void main(String[] args) {
-        recursion(4, 4);
+        recursion("", "bccadae");
     }
 
-    public static void recursion(int r, int c) {
-        if (r == 0) {
+    public static void recursion(String r, String c) {
+        if (c.isEmpty()) {
+            System.out.println(r);
             return;
         }
-        if (r < c) {
-            System.out.print("*");
-            recursion(r + 1, c);
+        char ch = c.charAt(0);
+        if (c.charAt(0) == 'a') {
+            recursion(r, c.substring(1));
         } else {
-            System.out.println();
-            recursion(0, c - 1);
+            recursion(r + ch, c.substring(1));
         }
     }
 }
---
+++ src/Charan/KunalDSA/Subsequence.java
@@ -0,0 +1,28 @@
+package Charan.KunalDSA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsequence {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        List<List<Integer>> list = subset(arr);
+        for (List<Integer> l : list) {
+            System.out.println(l);
+        }
+    }
+
+    public static List<List<Integer>> subset(int[] arr) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for (int i : arr) {
+            int n = outer.size();
+            for (int j = 0; j < n; j++) {
+                List<Integer> inner = new ArrayList<>(outer.get(j));
+                inner.add(i);
+                outer.add(inner);
+            }
+        }
+        return outer;
+    }
+}
