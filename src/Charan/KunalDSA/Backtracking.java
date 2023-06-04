b48c25af7269e330346b44a5c6926b7ce72e18cd
Charanvigneshnr <charanvignesh10@gmail.com>
2023-06-04 17:13:20 +0530

    Backtracking

---
+++ src/Charan/KunalDSA/Backtracking.java
@@ -0,0 +1,32 @@
+package Charan.KunalDSA;
+
+public class Backtracking {
+    public static void main(String[] args) {
+        boolean[][] matrix = {{true, true, true}, {true, true, true}, {true, true, true}};
+        totalPath("", matrix, 0, 0);
+    }
+
+    public static void totalPath(String p, boolean[][] arr, int r, int c) {
+        if (r == arr.length - 1 && c == arr[0].length - 1) {
+            System.out.println(p);
+            return;
+        }
+        if (!arr[r][c]) {
+            return;
+        }
+        arr[r][c] = false;
+        if (r < arr.length - 1) {
+            totalPath(p + "D ", arr, r + 1, c);
+        }
+        if (c < arr[0].length - 1) {
+            totalPath(p + "R ", arr, r, c + 1);
+        }
+        if (r > 0) {
+            totalPath(p + "U ", arr, r - 1, c);
+        }
+        if (c > 0) {
+            totalPath(p + "L ", arr, r, c - 1);
+        }
+        arr[r][c] = true;
+    }
+}
