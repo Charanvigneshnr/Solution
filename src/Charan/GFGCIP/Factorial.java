21a335cf859ed924de12b134e3c2e47249adf55d
Charanvigneshnr <charanvignesh10@gmail.com>
2023-06-22 21:15:11 +0530

    Reformatted

--- src/Charan/GFGCIP/Factorial.java
+++ src/Charan/GFGCIP/Factorial.java
@@ -4,11 +4,11 @@ public class Factorial {
     public static void main(String[] args) {
         System.out.println(factorial(5));
     }
+
     public static int factorial(int n) {
-        if (n==1)
-        {
+        if (n == 1) {
             return n;
         }
-        return n*factorial(n-1);
+        return n * factorial(n - 1);
     }
 }
--- src/Charan/GFGCIP/Patterns.java
+++ src/Charan/GFGCIP/Patterns.java
@@ -3,20 +3,21 @@ package Charan.GFGCIP;
 public class Patterns {
     public static void main(String[] args) {
         Patters(5);
-/*
-   *
-  ***
- *****
-*******
-*/
+        /*
+         *
+         ***
+         *****
+         *******
+         */
     }
-    public static void Patters(int n){
-        int i,j,k;
-        for (i=0;i<n;i++){
-            for (j=0;j<n-i-1;j++){
+
+    public static void Patters(int n) {
+        int i, j, k;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
             }
-            for (k=0;k<(2*(n-j))-1;k++){
+            for (k = 0; k < (2 * (n - j)) - 1; k++) {
                 System.out.print("*");
             }
             System.out.println();
