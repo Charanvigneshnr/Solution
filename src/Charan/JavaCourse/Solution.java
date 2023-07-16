515d3d3d99a1ee56fc5fe3b4967d0f2dfcecabe1
Charanvigneshnr <charanvignesh10@gmail.com>
2023-07-16 20:21:04 +0530

    JDBC reformatted

--- src/Charan/JavaCourse/Solution.java
+++ src/Charan/JavaCourse/Solution.java
@@ -2,10 +2,6 @@ package Charan.JavaCourse;
 
 public class Solution {
     public static void main(String[] args) {
-        for (Color clr : Color.values()) {
-            System.out.print(clr.name());
-            System.out.print(clr.getString());
-            System.out.println();
-        }
+
     }
 }
