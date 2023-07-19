6733b631379fa466414361481d7a1c8bff482a19
Charanvigneshnr <charanvignesh10@gmail.com>
2023-07-19 23:11:10 +0530

    Ranting

--- src/Charan/Test/IngaCheckPanra.java
+++ src/Charan/Test/IngaCheckPanra.java
@@ -1,11 +1,25 @@
 package Charan.Test;
 
-import Charan.KunalDSA.RBS;
-
 public class IngaCheckPanra {
     public static void main(String[] args) {
-        RBS rb = new RBS();
-        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
-        System.out.println(arr[rb.findPivot(arr)]);
+        System.out.println(isArmstrong(1635));
+        System.out.println(fact(10));
+    }
+
+    public static boolean isArmstrong(int n) {
+        int temp = n;
+        int num = 0;
+        int len = (int) Math.log10(n) + 1;
+        while (n > 0) {
+            num += Math.pow((n % 10), len);
+            n /= 10;
+        }
+        return num == temp;
+    }
+
+    public static int fact(int m) {
+        if (m == 0)
+            return 1;
+        return m * fact(m - 1);
     }
 }
