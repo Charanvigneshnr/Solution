5b74e1f374ab1482f92936b1fae7fb5f25e2c5c3
Charanvigneshnr <charanvignesh10@gmail.com>
2023-07-30 16:08:43 +0530

    Reformatted

--- src/Charan/Test/IngaCheckPanra.java
+++ src/Charan/Test/IngaCheckPanra.java
@@ -1,25 +1,10 @@
 package Charan.Test;
 
+import java.util.Arrays;
+
 public class IngaCheckPanra {
     public static void main(String[] args) {
-        System.out.println(isArmstrong(1635));
-        System.out.println(fact(10));
-    }
-
-    public static boolean isArmstrong(int n) {
-        int temp = n;
-        int num = 0;
-        int len = (int) Math.log10(n) + 1;
-        while (n > 0) {
-            num += Math.pow((n % 10), len);
-            n /= 10;
-        }
-        return num == temp;
-    }
-
-    public static int fact(int m) {
-        if (m == 0)
-            return 1;
-        return m * fact(m - 1);
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
     }
 }
