2200039d1b7bf5c5ea50f8faf1bdcfc5397b16d3
Charanvigneshnr <charanvignesh10@gmail.com>
2023-10-26 18:37:34 +0530

    Added too many files to keep track of

--- src/Charan/Test/LabRat.java
+++ src/Charan/Test/LabRat.java
@@ -1,22 +1,19 @@
 package Charan.Test;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class LabRat {
     public static void main(String[] args) {
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(1, "A");
-        map.put(2, "B");
-        map.put(3, "C");
-        map.put(4, "D");
-        map.put(5, "E");
-        map.put(6, "F");
-        int len = map.size();
-        int i;
-        for (i = 1; i <= len; i++) {
-            System.out.println(map.get(i));
-            System.out.println(map.containsValue((char) ('A' + (i - 1))));
+        int[] a1 = {1, 2, 5, 3, 2, 6, 7, 8};
+        int[] a2 = {4, 2, 3, 5, 4, 7, 9, 1};
+        int[] arr = new int[a1.length + a2.length];
+        System.arraycopy(a1, 0, arr, 0, a1.length);
+        System.arraycopy(a2, 0, arr, a1.length, a2.length);
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-        System.out.println((char) ('A' + 1));
+        System.out.println(map);
     }
 }
